306943be2a2b84d81c71ede5eea1b4a5b2148db3
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-03-14 23:06:45 +0530

    added the core feature in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="min-h-screen bg-zinc-900 text-zinc-50 flex items-center justify-center text-4xl">
-      App
+      Rankify
     </div>
   );
 };
